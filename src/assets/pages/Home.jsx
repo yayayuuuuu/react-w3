f809122f52f41c8e9a99b2653b051dd5974dd70a
yayayuuuuu <s111219031@stu.ntue.edu.tw>
2025-04-03 17:29:09 +0800

    h444444

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/Header.jsx";
-import Body from "./components/Body.jsx";
-import Footer from "./components/Footer.jsx";
+import Header from "../components/Header";  
+import Body from "../components/Body";      
+import Footer from "../components/Footer";  
 
 const Home = () => {
   return (
